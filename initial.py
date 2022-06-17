1f526bae5982b0ad49e6432cef66afb1b61d045b
YulinS-UoB <ok18185@bristol.ac.uk>
2022-06-17 02:52:13 +0100

    aa

--- initial.py
+++ initial.py
@@ -2,12 +2,14 @@ import os
 import pygame
 
 pygame.init()
-size = (700, 500)
-screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Fox & Crane')
+screen_main = pygame.display.set_mode((700, 500))
+screen_text = pygame.Surface((150, 100))
 myfont = pygame.font.Font("HanSans.ttf.ttc", 15)
-label = myfont.render("这是一段文本\n这是另一段文本", True, (122, 122, 150))
-screen.blit(label, (50, 20))
+label = myfont.render("这是一段文本", True, (210, 210, 215))
+text_Surf = screen_text.copy()
+text_Surf.blit(label, (0, 20))
+screen_main.blit(text_Surf, (50, 20))
 
 while True:
     for event in pygame.event.get():
